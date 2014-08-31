78f5ce163ef20dc0c6660682ead678783ba329f6
wooken <kenny@kennywoo.com>
2014-08-31 12:01:02 -0700

    add dunst support

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
-BASE=~/.dotfiles
+BASE=$HOME/.dotfiles
 CMD=ln
 OPT="-s"
 
 # Bash
-$CMD $OPT $BASE/bash/bashrc ~/.bashrc
+$CMD $OPT $BASE/bash/bashrc $HOME/.bashrc
 
 # fonts
-$CMD $OPT $BASE/fonts/ ~/.fonts
+$CMD $OPT $BASE/fonts/ $HOME/.fonts
 
 # Git
-$CMD $OPT $BASE/git/gitconfig ~/.gitconfig
+$CMD $OPT $BASE/git/gitconfig $HOME/.gitconfig
 
 # i3wm
-$CMD $OPT $BASE/i3wm/i3/ ~/.i3
-$CMD $OPT $BASE/i3wm/i3status.conf ~/.i3status.conf
+$CMD $OPT $BASE/i3wm/i3/ $HOME/.i3
+$CMD $OPT $BASE/i3wm/i3status.conf $HOME/.i3status.conf
 
 # Latex
-$CMD $OPT $BASE/latex/latexmkrc ~/.latexmkrc
+$CMD $OPT $BASE/latex/latexmkrc $HOME/.latexmkrc
 
 # Tmux
-$CMD $OPT $BASE/tmux/tmux.conf ~/.tmux.conf
+$CMD $OPT $BASE/tmux/tmux.conf $HOME/.tmux.conf
 
 # Vim
-$CMD $OPT $BASE/vim/vimrc ~/.vimrc
-$CMD $OPT $BASE/vim/dotvim/ ~/.vim
+$CMD $OPT $BASE/vim/vimrc $HOME/.vimrc
+$CMD $OPT $BASE/vim/dotvim/ $HOME/.vim
 
 # X11
-$CMD $OPT $BASE/x11/xinitrc ~/.xinitrc
-$CMD $OPT $BASE/x11/Xmodmap ~/.Xmodmap
-$CMD $OPT $BASE/x11/Xresources ~/.Xresources
+$CMD $OPT $BASE/x11/xinitrc $HOME/.xinitrc
+$CMD $OPT $BASE/x11/Xmodmap $HOME/.Xmodmap
+$CMD $OPT $BASE/x11/Xresources $HOME/.Xresources
+
+# dunst
+$CMD $OPT $BASE/dunst $HOME/.config/dunst
