7de7e0af2143a5623b13059a6d0f6faafa215ecf
wooken <kenny@kenwoo.io>
2015-06-19 09:22:34 -0700

    Migrate MBA startup scripts

---
+++ x11/config-synclient.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sleep 1
+synclient PalmMinWidth=10
+synclient PalmMinZ=20
+synclient HorizTwoFingerScroll=1
+synclient PalmDetect=1
+synclient MaxSpeed=3
+synclient MinSpeed=2
+synclient TapButton1=1
+synclient TapButton2=3
+synclient TapButton3=2
+synclient ClickFinger1=1
+synclient ClickFinger2=3
+synclient ClickFinger3=2
