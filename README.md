7fbab4510d476fce67f94d53cd9487778f433708
wooken <kenny@kenwoo.io>
2015-06-29 22:23:21 -0700

    Create script to deploy symlinks

--- README.md
+++ README.md
@@ -2,5 +2,4 @@ dotfiles
 ========
 
 To Do:
-- [ ] Create new python-based bootstrap script
 - [ ] Figure out how to deal with zsh and zprezto
---
+++ setup_symlinks.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+require 'fileutils'
+require 'find'
+require 'mkmf' #find_executable
+require 'yaml'
+
+def cmd?(cmd)
+  print "checking for #{cmd}... "
+  if system("which #{cmd} > /dev/null 2>&1")
+    puts "yes"
+    return true
+  else
+    puts "no"
+    return false
+  end
+end
+
+def error(msg)
+  puts "ERROR: " + msg
+end
+
+cfg_paths = []
+Find.find('.') do |path|
+  cfg_paths << path if path =~ /config\.yaml$/
+end
+
+cfg_paths.each do |cfg|
+  parsed = begin
+             YAML.load_file(cfg)
+           rescue ArgumentError => e
+             puts "Could not parse YAML: #{e.message}"
+           end
+  parsed.each do |file|
+    if cmd? file['cmd']
+      old = File.dirname(File.expand_path(cfg)) + '/' + file['src']
+      new = file['dest'].sub!('~', File.expand_path('~'))
+      if !File.exists?(old)
+        error "Could not find file: #{old}"
+      elsif File.exists?(new)
+        error "File already exists: #{new}"
+      else
+        puts "Creating symlink: #{old} => #{new}"
+        FileUtils.ln_s(old, new) if File.exists?(old) and !File.exists?(new)
+      end
+    end
+  end
+end
