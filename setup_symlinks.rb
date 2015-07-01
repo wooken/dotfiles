4d0eb94145a988032324039c4f68da7b16246dfc
wooken <kenny@kenwoo.io>
2015-06-30 23:24:19 -0700

    Complete symlink deploy script

--- setup_symlinks.rb
+++ setup_symlinks.rb
@@ -7,6 +7,15 @@ require 'yaml'
 $VALID_ACTIONS = %w(new deploy)
 
 # helper functions
+
+def usage
+  puts "Usage: #{$PROGRAM_NAME} " + $VALID_ACTIONS.join('|')
+end
+
+def error(msg)
+  puts "ERROR: " + msg
+end
+
 def cmd?(cmd)
   print "checking for #{cmd}... "
   if system("which #{cmd} > /dev/null 2>&1")
@@ -18,43 +27,49 @@ def cmd?(cmd)
   end
 end
 
-def error(msg)
-  puts "ERROR: " + msg
-end
-
-def usage
-  puts "Usage: #{$PROGRAM_NAME} " + $VALID_ACTIONS.join('|')
-end
-
 # execute
 if ARGV.empty?
-  usage()
+  usage
   exit
 end
 action = ARGV.shift
 
-cfg_paths = []
-Find.find('.') do |path|
-  cfg_paths << path if path =~ /config\.yaml$/
-end
+case action
+when 'new'
+when 'deploy'
+  cfg_paths = []
+  Find.find('.') do |path|
+    cfg_paths << path if path =~ /config\.yaml$/
+  end
 
-cfg_paths.each do |cfg|
-  parsed = begin
-             YAML.load_file(cfg)
-           rescue ArgumentError => e
-             puts "Could not parse YAML: #{e.message}"
-           end
-  parsed.each do |file|
-    if cmd?(file['cmd'])
-      old = File.dirname(File.expand_path(cfg)) + '/' + file['src']
-      new = file['dest'].sub!('~', File.expand_path('~'))
-      if !File.exists?(old)
-        error "Could not find file: #{old}"
-      elsif File.exists?(new)
-        error "File already exists: #{new}"
-      else
-        puts "Creating symlink: #{old} => #{new}"
-        FileUtils.ln_s(old, new) if File.exists?(old) and !File.exists?(new)
+  cfg_paths.each do |cfg|
+    parsed = begin
+               YAML.load_file(cfg)
+             rescue ArgumentError => e
+               puts "Could not parse YAML: #{e.message}"
+             end
+    parsed.each do |prog|
+      if cmd?(prog[0])
+        prog[1].each do |file|
+          if file['src'] =~ /~/
+            old = file['src'].sub!('~', File.expand_path('~'))
+          else
+            old = File.dirname(File.expand_path(cfg)) + '/' + file['src']
+          end
+          if file['dest'] =~ /~/
+            new = file['dest'].sub!('~', File.expand_path('~'))
+          else
+            new = file['dest']
+          end
+          if !File.exists?(old)
+            error "Could not find file: #{old}"
+          elsif File.exists?(new)
+            error "File already exists: #{new}"
+          else
+            puts "Creating symlink: #{old} => #{new}"
+            FileUtils.ln_s(old, new) if File.exists?(old) and !File.exists?(new)
+          end
+        end
       end
     end
   end
