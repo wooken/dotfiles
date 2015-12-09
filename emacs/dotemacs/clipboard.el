fd29c168ff41133363cec5b0aa532c1146e089a2
wooken <kenny@kenwoo.io>
2015-12-09 10:20:24 -0800

    Fix mixed indentation

--- emacs/dotemacs/clipboard.el
+++ emacs/dotemacs/clipboard.el
@@ -9,14 +9,14 @@
   (interactive)
   (if (display-graphic-p)
       (progn
-	(message "Yanked region to x-clipboard!")
-	(call-interactively 'clipboard-kill-ring-save)
-	)
+        (message "Yanked region to x-clipboard!")
+        (call-interactively 'clipboard-kill-ring-save)
+        )
     (if (region-active-p)
-	(progn
-	  (shell-command-on-region (region-beginning) (region-end) "xsel -i -b")
-	  (message "Yanked region to clipboard!")
-	  (deactivate-mark))
+        (progn
+          (shell-command-on-region (region-beginning) (region-end) "xsel -i -b")
+          (message "Yanked region to clipboard!")
+          (deactivate-mark))
       (message "No region active; can't yank to clipboard!")))
   )
 
@@ -25,9 +25,9 @@
   (interactive)
   (if (display-graphic-p)
       (progn
-	(clipboard-yank)
-	(message "graphics active")
-	)
+        (clipboard-yank)
+        (message "graphics active")
+        )
     (insert (shell-command-to-string "xsel -o -b"))
     )
   )
