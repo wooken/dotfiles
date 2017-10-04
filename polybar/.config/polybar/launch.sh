bbe66cb0b06e123932e262d020883452d76c7b2b
wooken <kenny@kenwoo.io>
2017-10-03 21:10:41 -0700

    Add polybar to VGA monitor

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,7 +6,8 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar unimatrix &
+# Launch bar
+MONITOR=eDP1 polybar unimatrix &
+MONITOR=DP2 polybar unimatrix &
 
 echo "Polybars launched..."
